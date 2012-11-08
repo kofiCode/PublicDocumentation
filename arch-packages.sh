aac35ed43ad1b18e929c31cac2570ea1b348486f
Fenton Travers <fenton.travers@oracle.com>
2012-11-08 13:02:49 +0700

    .

--- arch-packages.sh
+++
@@ -1,4 +0,0 @@
-sudo pacman -S --needed sudo alsa-utils xorg-server xorg-xinit xorg-server-utils mesa xorg-twm xorg-xclock xterm ttf-dejavu weechat chromium tree git emacs openssh rsync slim rxvt-unicode vlc autocutsel acpi netcfg net-tools dmenu
-
-
-
