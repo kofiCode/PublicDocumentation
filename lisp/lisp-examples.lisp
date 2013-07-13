81f2d6add5266aad02ecc894fee5918fc323f741
Fenton Travers <fenton.travers@oracle.com>
2013-07-13 11:36:27 +0700

    .

--- lisp/lisp-examples.lisp
+++ lisp/lisp-examples.lisp
@@ -25,4 +25,31 @@
 
 (get-all-symbols 'sb-thread)
 
+;; call an external program
+(sb-ext:run-program "/usr/bin/ls" (list "-l"))
 
+;; call an external program in a certain directory
+(uiop/filesystem:with-current-directory 
+    ("~") 
+  (uiop/run-program:run-program 
+   (list "git" "status") 
+   :output :string))
+
+
+;; PWD/CWD
+*DEFAULT-PATHNAME-DEFAULTS*
+;; => #P"/home/fenton/projects/lisp/urban-farmer/"
+(truename ".")
+;; => #P"/home/fenton/projects/lisp/urban-farmer/"
+
+;; changing directory
+(sb-posix:chdir #P"/home/fenton")
+;; => 0
+;; truename doesn't really work 
+(truename ".")
+;; => #P"/home/fenton/projects/lisp/urban-farmer/"
+
+
+;; Get an environment variable:
+(sb-unix::posix-getenv "HOME")
+;; => "/home/fenton"
